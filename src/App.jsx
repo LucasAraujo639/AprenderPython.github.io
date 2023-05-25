00d1e42ee087940c6523bdcf17a6dcc5cfadb10d
LucasAraujo639 <lucas.ezequiel.321@gmail.com>
2023-05-25 02:12:02 -0300

    'Prueba'

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AM2 from './components/service/AM2.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import style from './App.module.css'
 import FisicaI from './components/service/FisicaI';
+import Algebra from './components/service/Algebra';
 
 function App() {
   
@@ -13,7 +14,7 @@ function App() {
   return (
     
     <div className={`ml-0 mr-0 ${style.container}`}>
-      <h3 className='fw-bold text-decoration-underline text-center text-primary shadow-lg'>Fechas de parciales y finales de la FIUBA</h3>
+      <h3 className='fw-bold text-decoration-underline text-center text-primary'>Fechas de parciales y finales de la FIUBA</h3>
       <Card title="AM2" 
       body={<BodyCard data={AM2}/>}
       /> 
@@ -22,6 +23,9 @@ function App() {
       body={<BodyCard data={FisicaI}/>}
       /> 
 
+      <Card title="Algebra II" 
+      body={<BodyCard data={Algebra}/>}
+      /> 
       <Footer />
     </div>
     
